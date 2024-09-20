6ce580fa2688e986952204346340857919e98067
Tobias Oitzinger <42447585+toitzi@users.noreply.github.com>
2024-09-20 10:46:24 +0200

    Fix asset publishing

--- ApiPlatformProvider.php
+++ ApiPlatformProvider.php
@@ -1210,11 +1210,11 @@ class ApiPlatformProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/config/api-platform.php' => $this->app->configPath('api-platform.php'),
-            ], 'laravel-assets');
+            ], 'api-platform-config');
 
             $this->publishes([
                 __DIR__.'/public' => $this->app->publicPath('vendor/api-platform'),
-            ], 'laravel-assets');
+            ], ['api-platform-assets', 'public']);
         }
 
         $this->loadViewsFrom(__DIR__.'/resources/views', 'api-platform');
